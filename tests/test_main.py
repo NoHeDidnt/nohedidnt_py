bb245135c3653c17dc7c1d8921058895b9fc8339
NoHeDidnt <dj@nohedidnt.de>
2023-02-20 05:31:18 +0100

    General improvements

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,5 +2,19 @@
 from tests.context import *
 
 
-def test_nhd_debug():
-    assert nhd_debug("Test Message") is None
+def test_format_msg(msg_string):
+    formatted_msg = format_msg(msg_string)
+    
+    assert isinstance(formatted_msg, str)
+    assert msg_string in formatted_msg
+    assert PKG_ABBR in formatted_msg
+
+
+def test_nhd_debug(msg_string):
+    """
+        GIVEN msg_string
+        WHEN nhd_debug is called
+        THEN None is returned
+    """
+    
+    assert nhd_debug(msg_string) is None
